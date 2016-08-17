aaf1015b8b32f894e12edc680095514b28f96072
Taylor Widman <tpwidman@gmail.com>
2016-08-17 16:54:41 -0500

    routing setup

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,64 +1,27 @@
 import {Component} from 'angular2/core';
 import {RouteConfig} from 'angular2/router';
-import {ContainerComponent} from './container.component';
 
 import {HomeComponent} from './home/home.component';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
 import {BlogComponent} from './blog/blog.component'
-@RouteConfig([
-    { path: '/home', name:'Home', component:HomeComponent, useAsDefault: true},
-    { path: '/about', name:'About', component:AboutComponent},
-    { path: '/contact', name:'Contact', component:ContactComponent},
-    { path: '/blog', name: 'Blog', component:BlogComponent},
-    { path: '/*other', name:'Other', redirectTo: ['Home'] }
-])
 
 @Component({
     selector: 'hospital-app',
-    templateURL: '/home/home.html',
+    template: require('/home/home.html'),
+    styles: [require('/styles/styles.css')]
     directives: [
-        ContainerComponent
-    ]
+        ROUTER_DIRECTIVES
+    ],
+    providers : [StateService, BlogService]
 })
-export class AppComponent{
 
-}
+@RouteConfig([
+    { path: '/home', name:'Home',       component:HomeComponent, useAsDefault: true},
+    { path: '/about', name:'About',     component:AboutComponent},
+    { path: '/contact', name:'Contact', component:ContactComponent},
+    { path: '/blog', name: 'Blog',      component:BlogComponent},
+    { path: '/*other', name:'Other',    redirectTo: ['Home'] }
+])
 
-
-
-
-// var joeApp = angular
-
-//     .module('joeApp', ['ui.router']);
-
-//     joeApp.config(function ($stateProvider, $urlProvider) {
-
-//     $urlRouteProvider.otherwise('/');
-
-//     $stateProvider
-
-//         .state('home', {
-//             url: "/home",
-//             templateUrl: "views/home/home.html"
-//         })
-//         .state('home.list', {
-//             url:'/list',
-//             templateurl: 'views/home/home-list.html',
-//             controller: function($scope) {
-//                 $scope.items = ['A', 'list', 'of', 'items'];
-//             }
-//         })
-//         .state('blog', {
-//             url: "/blog",
-//             templateUrl: "views/blog/blog.html"
-//         })
-//         .state('contact', {
-//             url:'/contact',
-//             templateurl: 'views/contact/contact.html'
-//         })
-//         .state('about', {
-//             url: "/about",
-//             templateUrl: "views/about/about.html"
-//         })
-//     })
+export class AppComponent{}
