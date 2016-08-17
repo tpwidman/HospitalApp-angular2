c1fd570bac16c3dac81c85f0990d6d8554cb5440
Taylor Widman <tpwidman@gmail.com>
2016-08-17 16:38:01 -0500

    renaming some stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,14 +2,15 @@ import {Component} from 'angular2/core';
 import {RouteConfig} from 'angular2/router';
 import {ContainerComponent} from './container.component';
 
-import {HomeComponent} from './home.component';
-import {AboutComponent} from './about.component';
-import {ContactComponent} from './contact.component'
-
+import {HomeComponent} from './home/home.component';
+import {AboutComponent} from './about/about.component';
+import {ContactComponent} from './contact/contact.component';
+import {BlogComponent} from './blog/blog.component'
 @RouteConfig([
     { path: '/home', name:'Home', component:HomeComponent, useAsDefault: true},
     { path: '/about', name:'About', component:AboutComponent},
     { path: '/contact', name:'Contact', component:ContactComponent},
+    { path: '/blog', name: 'Blog', component:BlogComponent},
     { path: '/*other', name:'Other', redirectTo: ['Home'] }
 ])
 
