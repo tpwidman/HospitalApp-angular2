b552938df822b3d7c387c26a2d97097e55c2d3d2
Taylor Widman <tpwidman@gmail.com>
2016-08-17 11:40:17 -0500

    added updated angular2 scripts

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,34 +1,48 @@
-var joeApp = angular
+import {Component} from 'angular2/core';
+import {ContainerComponent} from './container.component'
+@Component({
+    selector: 'hospital-app',
+    template: '<h1>Hello world!</h1><container></container>',
+    directives: [
+        
+    ]
+})
+export class AppComponent{}
 
-    .module('joeApp', ['ui.router']);
 
-    joeApp.config(function ($stateProvider, $urlProvider) {
 
-    $urlRouteProvider.otherwise('/');
 
-    $stateProvider
+// var joeApp = angular
 
-        .state('home', {
-            url: "/home",
-            templateUrl: "views/home/home.html"
-        })
-        .state('home.list', {
-            url:'/list',
-            templateurl: 'views/home/home-list.html',
-            controller: function($scope) {
-                $scope.items = ['A', 'list', 'of', 'items'];
-            }
-        })
-        .state('blog', {
-            url: "/blog",
-            templateUrl: "views/blog/blog.html"
-        })
-        .state('contact', {
-            url:'/contact',
-            templateurl: 'views/contact/contact.html'
-        })
-        .state('about', {
-            url: "/about",
-            templateUrl: "views/about/about.html"
-        })
-    })
+//     .module('joeApp', ['ui.router']);
+
+//     joeApp.config(function ($stateProvider, $urlProvider) {
+
+//     $urlRouteProvider.otherwise('/');
+
+//     $stateProvider
+
+//         .state('home', {
+//             url: "/home",
+//             templateUrl: "views/home/home.html"
+//         })
+//         .state('home.list', {
+//             url:'/list',
+//             templateurl: 'views/home/home-list.html',
+//             controller: function($scope) {
+//                 $scope.items = ['A', 'list', 'of', 'items'];
+//             }
+//         })
+//         .state('blog', {
+//             url: "/blog",
+//             templateUrl: "views/blog/blog.html"
+//         })
+//         .state('contact', {
+//             url:'/contact',
+//             templateurl: 'views/contact/contact.html'
+//         })
+//         .state('about', {
+//             url: "/about",
+//             templateUrl: "views/about/about.html"
+//         })
+//     })
---
+++ app/container.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector: 'containers',
+    template: '<h2>Hello</h2>'
+})
+export class ContainerComponent{
+
+}
