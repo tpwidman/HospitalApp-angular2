1657efd42d9ddc47e741632ceeacc4ac41e34300
Taylor Widman <tpwidman@gmail.com>
2016-08-20 17:57:33 -0500

    added dependencies

---
+++ main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser' // import bootstrap
+import {AppComponent} from './app.component' // import the component we just created
+
+bootstrap(AppComponent); // finally bootstrap it.  
