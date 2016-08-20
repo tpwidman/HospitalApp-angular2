176f53f7d640c1701348a3bba4d1688fe68c8c7d
Taylor Widman <tpwidman@gmail.com>
2016-08-20 17:52:37 -0500

    adding dependencies

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,12 +5,19 @@ import {CardComponent} from './home/sample-rating-card/card.component'
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
 import {BlogComponent} from './blog/blog.component';
+import {DetailComponent} from './blog/blog-details/blog-detail.component'
 
 // Route Configuration
 export const routes: RouterConfig = [
-    { path: '/home', name:'Home',       component:{HomeComponent, CardComponent} useAsDefault: true},
-    { path: '/about', name:'About',     component:AboutComponent},
-    { path: '/contact', name:'Contact', component:ContactComponent},
-    { path: '/blog', name: 'Blog',      component:BlogComponent},
+    { path: '/home', name:'Home',       component:{HomeComponent, CardComponent}, useAsDefault: true},
+    { path: '/about', name:'About',     component: AboutComponent },
+    { path: '/contact', name:'Contact', component: ContactComponent },
+    { path: '/blog', name: 'Blog',      component:{BlogComponent, DetailComponent} },
     { path: '/*other', name:'Other',    redirectTo: ['Home'] }
 ]
+
+// Export routes
+
+export const APP_ROUTER_PROVIDERS = [
+    provideRouter(routes)
+];
---
+++ app/home/home.component.ts
@@ -0,0 +1,9 @@
+// Import component decorator
+import { Component } from '@angular/core';
+
+@Component({
+    template:`<h2>Home</h2>`
+})
+export class HomeComponent{}
+
+
