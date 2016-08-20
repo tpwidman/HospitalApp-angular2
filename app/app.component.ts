8dd522761578e26323cd6b362080e9e67dd1af36
Taylor Widman <tpwidman@gmail.com>
2016-08-20 18:49:42 -0500

    not sure what's wrong

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,21 +3,24 @@ import {ROUTER_DIRECTIVES} from '@angular/router';
 import {RouteConfig} from 'angular2/router';
 
 import {HomeComponent} from './home/home.component';
-import {CardComponent} from './home/sample-rating-card/card.component'
+// import {CardComponent} from './home/sample-rating-card/card.component'
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
 import {BlogComponent} from './blog/blog.component';
-import {DetailComponent} from './blog/blog-details/blog-detail.component'
+// import {DetailComponent} from './blog/blog-details/blog-detail.component'
 
 @Component({
     selector: 'hospital-app',
-    template: ('/home/home.component.html'),
+    template: `<div class="hospital-app">
+                Hello!
+                <main></main>
+                </div>`,
     styles: [('/styles/styles.css')],
     directives: [ROUTER_DIRECTIVES]
 })
 
 @RouteConfig([
-    { path: '/home', name:'Home',       component:{HomeComponent, CardComponent}, useAsDefault: true},
+    { path: '/home', name:'Home',       component:HomeComponent, useAsDefault: true},
     { path: '/about', name:'About',     component:AboutComponent},
     { path: '/contact', name:'Contact', component:ContactComponent},
     { path: '/blog', name: 'Blog',      component:BlogComponent},
--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,6 @@
 import {bootstrap}    from 'angular2/platform/browser' // import bootstrap
 import {AppComponent} from './app.component' // import the component we just created
-import { APP_ROUTER_PROVIDERS } from '@angular/router';
+import { APP_ROUTER_PROVIDERS } from './app.routes';
 // finally bootstrap it. 
 bootstrap(AppComponent, [
     APP_ROUTER_PROVIDERS
