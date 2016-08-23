1f081421540141cec0c83d3b8c7603ba9128980b
tpwidman <tpwidman@gmail.com>
2016-08-23 14:53:23 -0500

    added method

--- app/home/sample-rating-card/card.service.ts
+++ app/home/sample-rating-card/card.service.ts
@@ -1,5 +1,17 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 
-export CardService{}
+export class CardService{
+    
+    constructor(private http: Http){
+
+    }
+
+    getHospitals(){
+        return this.http.get('hospitals.json')
+                   .map(response => Hospital[]response.json().data);
+    }
+}
