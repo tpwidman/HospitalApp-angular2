abed44b601bc79430ebb5762cc72bfcc1408488c
tpwidman <tpwidman@gmail.com>
2016-08-23 01:49:57 -0500

    added navbar

---
+++ app/shared/navbar/navbar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'navbar',
+    templateUrl: 'navvar.component.html',
+    styleUrls: ['navbar.component.css'],
+})
+export class NavbarComponent {}
